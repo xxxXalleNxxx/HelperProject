d2bc2f7864873724daf59716a91dd8714ce53b3d
xallen <arapov.artem.2003@mail.ru>
2023-10-24 20:24:45 +0300

    added user controller with mappings

---
+++ src/main/java/ru/arapov/helperproject/HelperProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.arapov.helperproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelperProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelperProjectApplication.class, args);
+    }
+
+}
